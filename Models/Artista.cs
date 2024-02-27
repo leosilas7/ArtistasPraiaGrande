ebc83c750d44be55d473fa5a1e990a6b94f7c6f6
Leonardo Silas <151047455+leosilas7@users.noreply.github.com>
2024-02-27 14:07:06 -0300

    Initial commit

---
+++ Models/Artista.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ArtistasPraiaGrande.Models
+{
+    public partial class Artista
+    {
+        public int IdArtista { get; set; }
+        public string NomeCompleto { get; set; }
+        public string NomeSocial { get; set; }
+        public string NomeArtistico { get; set; }
+        public byte Ativo { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Modified { get; set; }
+    }
+}
